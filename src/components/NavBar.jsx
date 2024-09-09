8e54d95a28d5b63996c9dd720f56678f90435451
SyedArbaazHussain <arbaaz14122002@gmail.com>
2024-09-09 18:52:58 +0530

    Navbar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,29 +26,38 @@ const NavBar = () => {
 
   return (
     <nav className="sticky top-0 flex w-full bg-green-600 justify-center items-center h-[10vh]">
-      <div className="flex w-4/5 bg-red-600 text-center items-center gap-5 h-4/5">
+      <div className="flex w-screen h-full xl:w-4/5 xl:h-4/5 bg-red-600 text-center items-center gap-5 ">
         <div className="h-full">
-          <img className="h-full" src="/me-head-circle.png" alt="My Image" />
+          <img
+            className="h-full bg-yellow-700"
+            src="/me-head-circle.png"
+            alt="My Image"
+          />
         </div>
-        <div className="flex justify-center items-center h-full flex-1">
+        <div className="flex justify-center items-center h-full flex-1 bg-yellow-700">
           <div className="text-xl">SYED ARBAAZ HUSSAIN</div>
         </div>
-        <div className="hidden md:flex md:flex-row flex-grow">
+        <div className="hidden flex-grow xl:flex xl:flex-row xl:flex-grow ">
           <div className="bg-slate-500 flex-grow">3</div>
+          <div className="bg-slate-500 flex-grow">23</div>
+          <div className="bg-slate-500 flex-grow">33</div>
           <div className="bg-slate-400 flex-grow">4</div>
           <div className="bg-slate-300 flex-grow">5</div>
+          <div className="bg-slate-300 flex-grow">56</div>
+          <div className="bg-slate-300 flex-grow">57</div>
+          <div className="bg-slate-300 flex-grow">58</div>
         </div>
-        <div className="flex h-full w-fit md:hidden">
+        <div className="flex h-full w-fit xl:hidden bg-yellow-700">
           <button
-            className="relative flex items-center justify-center rounded-full w-[50px] h-[50px] bg-slate-700 ring-0 ring-gray-300 hover:ring-8 focus:ring-4 ring-opacity-30 transition duration-200 shadow-md"
+            className="relative flex items-center content-center justify-center rounded-full mx-2 h-full  bg-slate-700 ring-0 ring-gray-300 hover:ring-8 focus:ring-4 ring-opacity-30 transition duration-200 shadow-xl"
             onClick={toggleMenu}
             aria-expanded={isMenuOpen}
             aria-label="Toggle menu"
           >
-            <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center">
-              <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg] group-focus:w-2/3 delay-150"></div>
-              <div className="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
-              <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg] group-focus:w-2/3 delay-150"></div>
+            <div className="flex flex-col justify-between w-6 h-6 transform transition-all duration-300 origin-center ">
+              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 origin-left group-focus:rotate-45 group-focus:w-2/3 delay-150"></div>
+              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
+              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 origin-left group-focus:-rotate-45 group-focus:w-2/3 delay-150"></div>
             </div>
           </button>
         </div>
@@ -58,11 +67,16 @@ const NavBar = () => {
       {isMenuOpen && (
         <div
           ref={menuRef}
-          className="fixed md:hidden flex flex-col h-screen w-[80vw] left-[10vw] top-[10vh] bg-white z-50"
+          className="fixed xl:hidden flex flex-col h-fit w-[80vw] top-[10vh] bg-gray-700 z-50"
         >
-          <div className="bg-slate-500">3</div>
-          <div className="bg-slate-400">4</div>
-          <div className="bg-slate-300">5</div>
+          <div className="bg-slate-500 flex-grow">3</div>
+          <div className="bg-slate-500 flex-grow">23</div>
+          <div className="bg-slate-500 flex-grow">33</div>
+          <div className="bg-slate-400 flex-grow">4</div>
+          <div className="bg-slate-300 flex-grow">5</div>
+          <div className="bg-slate-300 flex-grow">56</div>
+          <div className="bg-slate-300 flex-grow">57</div>
+          <div className="bg-slate-300 flex-grow">58</div>
         </div>
       )}
     </nav>
