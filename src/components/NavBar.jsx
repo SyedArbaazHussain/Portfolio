647da5895cab379139a88f1226945481b680a642
SyedArbaazHussain <arbaaz14122002@gmail.com>
2024-09-09 19:37:17 +0530

    improvements to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,38 +26,42 @@ const NavBar = () => {
 
   return (
     <nav className="sticky top-0 flex w-full bg-green-600 justify-center items-center h-[10vh]">
-      <div className="flex w-screen h-full xl:w-4/5 xl:h-4/5 bg-red-600 text-center items-center gap-5 ">
-        <div className="h-full">
+      <div className="flex w-[90vw] h-full xl:w-[80vw] xl:h-full bg-red-600 text-center items-center gap-5 ">
+        <div className="h-full flex-shrink-0 justify-center items-center content-center">
           <img
-            className="h-full bg-yellow-700"
+            className="h-5/6 bg-yellow-700"
             src="/me-head-circle.png"
             alt="My Image"
           />
         </div>
         <div className="flex justify-center items-center h-full flex-1 bg-yellow-700">
-          <div className="text-xl">SYED ARBAAZ HUSSAIN</div>
+          <div className="text-3xl text-nowrap">SYED ARBAAZ HUSSAIN</div>
         </div>
-        <div className="hidden flex-grow xl:flex xl:flex-row xl:flex-grow ">
-          <div className="bg-slate-500 flex-grow">3</div>
-          <div className="bg-slate-500 flex-grow">23</div>
-          <div className="bg-slate-500 flex-grow">33</div>
-          <div className="bg-slate-400 flex-grow">4</div>
-          <div className="bg-slate-300 flex-grow">5</div>
-          <div className="bg-slate-300 flex-grow">56</div>
-          <div className="bg-slate-300 flex-grow">57</div>
-          <div className="bg-slate-300 flex-grow">58</div>
+        <div className="hidden flex-grow xl:flex xl:flex-row xl:flex-grow h-4/6 justify-center content-center">
+          <div className="bg-slate-500 flex-grow justify-center content-center text-center text-xl">
+            HOME
+          </div>
+          <div className="bg-slate-400 flex-grow justify-center content-center text-center text-xl">
+            ABOUT
+          </div>
+          <div className="bg-slate-300 flex-grow justify-center content-center text-center text-xl">
+            PROJECTS
+          </div>
+          <div className="bg-slate-600 flex-grow justify-center content-center text-center text-xl">
+            CONTACT
+          </div>
         </div>
-        <div className="flex h-full w-fit xl:hidden bg-yellow-700">
+        <div className="flex flex-shrink max-h-full h-4/6 min-w-24 max-w-28 xl:hidden bg-yellow-300">
           <button
-            className="relative flex items-center content-center justify-center rounded-full mx-2 h-full  bg-slate-700 ring-0 ring-gray-300 hover:ring-8 focus:ring-4 ring-opacity-30 transition duration-200 shadow-xl"
+            className="relative flex items-center content-center justify-center rounded-full mx-2 h-full w-full bg-slate-700 ring-0 ring-gray-300 hover:ring-8 focus:ring-4 ring-opacity-30 transition duration-200 shadow-xl"
             onClick={toggleMenu}
             aria-expanded={isMenuOpen}
             aria-label="Toggle menu"
           >
-            <div className="flex flex-col justify-between w-6 h-6 transform transition-all duration-300 origin-center ">
-              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 origin-left group-focus:rotate-45 group-focus:w-2/3 delay-150"></div>
-              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
-              <div className="bg-white h-[4px] w-full rounded transform transition-all duration-300 origin-left group-focus:-rotate-45 group-focus:w-2/3 delay-150"></div>
+            <div className="flex flex-col justify-center w-full h-full transform transition-all duration-300 origin-center items-center">
+              <div className="bg-white h-[4px] w-3/6 my-[3px] py-[4px]  rounded transform transition-all duration-300 origin-left group-focus:rotate-45 group-focus:w-2/3 delay-150"></div>
+              <div className="bg-white h-[4px] w-3/6 my-[3px] py-[4px]  rounded transform transition-all duration-300 group-focus:translate-x-10"></div>
+              <div className="bg-white h-[4px] w-3/6 my-[3px] py-[4px]  rounded transform transition-all duration-300 origin-left group-focus:-rotate-45 group-focus:w-2/3 delay-150"></div>
             </div>
           </button>
         </div>
@@ -67,16 +71,20 @@ const NavBar = () => {
       {isMenuOpen && (
         <div
           ref={menuRef}
-          className="fixed xl:hidden flex flex-col h-fit w-[80vw] top-[10vh] bg-gray-700 z-50"
+          className="fixed xl:hidden flex flex-col h-[30%] w-[90vw] top-[10vh] bg-gray-700 z-50"
         >
-          <div className="bg-slate-500 flex-grow">3</div>
-          <div className="bg-slate-500 flex-grow">23</div>
-          <div className="bg-slate-500 flex-grow">33</div>
-          <div className="bg-slate-400 flex-grow">4</div>
-          <div className="bg-slate-300 flex-grow">5</div>
-          <div className="bg-slate-300 flex-grow">56</div>
-          <div className="bg-slate-300 flex-grow">57</div>
-          <div className="bg-slate-300 flex-grow">58</div>
+          <div className="bg-slate-500 flex-grow justify-center content-center text-center text-3xl">
+            HOME
+          </div>
+          <div className="bg-slate-400 flex-grow justify-center content-center text-center text-3xl">
+            ABOUT
+          </div>
+          <div className="bg-slate-300 flex-grow justify-center content-center text-center text-3xl">
+            PROJECTS
+          </div>
+          <div className="bg-slate-600 flex-grow justify-center content-center text-center text-3xl">
+            CONTACT
+          </div>
         </div>
       )}
     </nav>
