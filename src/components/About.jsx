bdfea01ae01e3e0a37b71132ad8519a1cc76d555
SyedArbaazHussain <arbaaz14122002@gmail.com>
2024-09-21 19:04:50 +0530

    fix navbar dark and light modes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='h-[100vh] pt-[9vh]'>
-      <h1 className='text-2xl font-bold'>About Us</h1>
+    <div className='h-[100vh] pt-[14vh]'>
+      <h1 className='text-2xl font-bold'>About Me</h1>
       <p className='mt-4'>
         Welcome to our About page. Here you can add more information about your company or project.
       </p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import headImg from '/me-head-circle-thumb.png'
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const menuRef = useRef(null);
 
   const toggleMenu = () => {
@@ -44,7 +44,7 @@ const NavBar = () => {
               src={headImg}
               alt="My Image"
             />
-            <div className="ml-4 text-xl font-bold text-black sm:text-xl md:text-2xl">
+            <div className="ml-4 text-xl font-bold text-black sm:text-xl md:text-2xl dark:text-white">
               SYED ARBAAZ HUSSAIN
             </div>
           </div>
@@ -52,13 +52,13 @@ const NavBar = () => {
           {/* Desktop Menu */}
           <div className="hidden md:flex md:max-lg:space-x-1 md:space-x-4">
             <div className="hidden md:flex md:max-lg:space-x-1 md:space-x-4">
-              <button onClick={() => window.location.href='#home'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium text-black sm:text-sm md:text-base">HOME</button>
-              <button onClick={() => window.location.href='#about'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium text-black sm:text-sm md:text-base">ABOUT</button>
-              <button onClick={() => window.location.href='#projects'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium text-black sm:text-sm md:text-base">PROJECTS</button>
-              <button onClick={() => window.location.href='#contact'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium text-black sm:text-sm md:text-base">CONTACT</button>
+              <button onClick={() => window.location.href='#home'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium dark:text-white text-black sm:text-sm md:text-base">HOME</button>
+              <button onClick={() => window.location.href='#about'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium dark:text-white text-black sm:text-sm md:text-base">ABOUT</button>
+              <button onClick={() => window.location.href='#projects'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium dark:text-white text-black sm:text-sm md:text-base">PROJECTS</button>
+              <button onClick={() => window.location.href='#contact'} className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium dark:text-white text-black sm:text-sm md:text-base">CONTACT</button>
               <button
             onClick={toggleDarkMode}
-            className="bg-blue-500 dark:bg-blue-700 px-3 py-2 rounded-full text-xs font-medium text-black dark:text-white sm:text-sm md:text-base"
+            className="bg-blue-500 px-3 py-2 rounded-full text-xs font-medium text-black dark:text-white sm:text-sm md:text-base"
           >
             {isDarkMode ? 'Light Mode' : 'Dark Mode'}
           </button>
@@ -67,7 +67,7 @@ const NavBar = () => {
           
           {/* Mobile Menu Button */}
           <button
-            className="md:hidden p-2 rounded-md text-black hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-white"
+            className="md:hidden p-2 rounded-full text-black hover:bg-blue-500 bg-blue-500 focus:outline-none focus:ring-2 focus:ring-white"
             onClick={toggleMenu}
             aria-expanded={isMenuOpen}
             aria-label="Toggle menu"
@@ -82,13 +82,13 @@ const NavBar = () => {
         {isMenuOpen && (
         <div
           ref={menuRef}
-          className="md:hidden absolute top-16 inset-x-0 bg-white dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-md"
+          className="md:hidden absolute top-50 inset-x-0 bg-white dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-md  rounded-3xl"
         >
           <div className="px-2 pt-2 pb-3 space-y-1">
-              <button onClick={() => window.location.href='#home'} className="block w-full text-left px-3 py-2 text-base font-medium text-black hover:bg-gray-200">HOME</button>
-              <button onClick={() => window.location.href='#about'} className="block w-full text-left px-3 py-2 text-base font-medium text-black hover:bg-gray-200">ABOUT</button>
-              <button onClick={() => window.location.href='#projects'} className="block w-full text-left px-3 py-2 text-base font-medium text-black hover:bg-gray-200">PROJECTS</button>
-              <button onClick={() => window.location.href='#contact'} className="block w-full text-left px-3 py-2 text-base font-medium text-black hover:bg-gray-200">CONTACT</button>
+              <button onClick={() => window.location.href='#home'} className="block w-full text-left px-3 py-2 text-base font-medium text-black dark:text-white hover:bg-gray-200">HOME</button>
+              <button onClick={() => window.location.href='#about'} className="block w-full text-left px-3 py-2 text-base font-medium text-black dark:text-white hover:bg-gray-200">ABOUT</button>
+              <button onClick={() => window.location.href='#projects'} className="block w-full text-left px-3 py-2 text-base font-medium text-black dark:text-white hover:bg-gray-200">PROJECTS</button>
+              <button onClick={() => window.location.href='#contact'} className="block w-full text-left px-3 py-2 text-base font-medium text-black dark:text-white hover:bg-gray-200">CONTACT</button>
               <button
               onClick={toggleDarkMode}
               className="block w-full text-left px-3 py-2 text-base font-medium text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
