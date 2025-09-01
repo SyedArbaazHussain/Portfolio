fe882bc0eb978af0710e7f2b4c896235cb8cbfc0
SyedArbaazHussain <arbaaz14122002@gmail.com>
2025-09-01 17:15:22 +0530

    try fixing custom cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: { 
-      cursor: {
-        CustomLight: 'url(src/assets/cursor-light.svg), auto',
-        CustomDark: 'url(src/assets/cursor-dark.svg), auto'
-      }
     },
   },
   plugins: [],
